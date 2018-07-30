438c3dc479a5eb51a9a6524f37b39a90ecd3fa00
Gertjan van Heertum <git@netietsmeer.nl>
2018-07-30 22:14:06 +0200

    Initial project commit

---
+++ Library/QuoteFileReader.cs
@@ -0,0 +1,16 @@
+namespace QuoteClock.Library
+{
+	public class QuoteElement
+	{
+		public string TimeString {get;set;}
+	}
+
+	public class QuoteContainer 
+	{
+		
+	}
+	public class QuoteFileReader
+	{
+
+	}
+}
