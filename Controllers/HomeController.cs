ac2f8a038ff15167b89bc65cfdc9fddb8858f4a4
Gertjan van Heertum <git@netietsmeer.nl>
2018-07-30 22:44:50 +0200

    Reading the file from the wwwroot and first simple parsing of the content file

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,17 +2,40 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Collections;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
 
 namespace QuoteClock.Controllers
 {
     public class HomeController : Controller
     {
+		private IHostingEnvironment _env;
+        public HomeController(IHostingEnvironment env)
+        {
+            _env = env;
+        }	
+
         public IActionResult Index()
         {
             return View();
         }
+
+		public string Quotes()
+		{
+			var quotes = new Library.QuoteFileReader(GetQuoteFileLocation()).ReadQuotes();
+			System.Text.StringBuilder sb = new System.Text.StringBuilder();
+			quotes.Take(100).ToList().ForEach(i => sb.AppendLine(i.Quote));
+			return sb.ToString();
+		}
+
+		private string GetQuoteFileLocation()
+		{
+			var webRoot = _env.WebRootPath;
+            var file = System.IO.Path.Combine(webRoot, "timeqoutes.txt");
+			return file;
+		}
 
         public IActionResult Error()
         {
--- Library/QuoteFileReader.cs
+++ Library/QuoteFileReader.cs
@@ -1,8 +1,19 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
 namespace QuoteClock.Library
 {
 	public class QuoteElement
 	{
 		public string TimeString {get;set;}
+		public string TimeStringInQuote {get;set;}
+		public int Hour { get;set;}
+		public int Minute {get;set;}
+		public string Quote {get;set;}
+		public string Title {get;set;}
+		public string Author {get;set;}
 	}
 
 	public class QuoteContainer 
@@ -11,6 +22,36 @@ namespace QuoteClock.Library
 	}
 	public class QuoteFileReader
 	{
+		private string _fileName;
+		public QuoteFileReader(string fileName)
+		{
+			_fileName = fileName;
+		}
 
+		public List<QuoteElement> ReadQuotes()
+		{
+			var lines = System.IO.File.ReadAllLines(_fileName);
+			return lines.Select(GetQuoteElementFromLine).ToList();
+		}
+
+		private QuoteElement GetQuoteElementFromLine(string line)
+		{
+			string[] spl = line.Split(new [] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+			
+			return new QuoteElement() 
+			{ 
+				TimeString = GetPartFromSplitted(spl, 0, ""),
+				TimeStringInQuote = GetPartFromSplitted(spl, 1, "?"),
+				Quote = GetPartFromSplitted(spl, 2, "No-Quote"),
+				Title = GetPartFromSplitted(spl, 3, "No Title"),
+				Author = GetPartFromSplitted(spl, 4, "No Author"),
+			};
+		}
+
+		private string GetPartFromSplitted(string[] splitted, int index, string defaultText)
+		{
+			if(splitted.Length <= index || string.IsNullOrWhiteSpace(splitted[index])) { return defaultText; }
+			return splitted[index];
+		}
 	}
 }
