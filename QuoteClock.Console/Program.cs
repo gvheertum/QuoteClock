63ed6ce4a0e468f0bf5c1320e5573d1844676b6e
Gertjan van Heertum <gertjan.van.heertum@tax-model.com>
2018-08-20 11:23:30 +0200

    Also made working for windows

--- QuoteClock.Console/Program.cs
+++ QuoteClock.Console/Program.cs
@@ -17,9 +17,13 @@ namespace QuoteClock.Console
 		private static string GetQuoteFilePath()
 		{
 			string determinedPath = AppContext.BaseDirectory;
-			if(determinedPath.IndexOf("bin/Debug") > -1) 
+			if(determinedPath.IndexOf("/bin/") > -1) 
 			{ 
-				determinedPath = determinedPath.Substring(0, determinedPath.IndexOf("/bin/Debug")); 
+				determinedPath = determinedPath.Substring(0, determinedPath.IndexOf("/bin/")); 
+			}
+			if(determinedPath.IndexOf("\\bin\\") > -1) 
+			{ 
+				determinedPath = determinedPath.Substring(0, determinedPath.IndexOf("\\bin\\")); 
 			}
 			System.Console.WriteLine($"We think the quotes are at: {determinedPath}");
 			determinedPath = System.IO.Path.Combine(determinedPath, "timequotes.txt");
